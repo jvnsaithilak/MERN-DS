aaad73fc1ac47670659b6acd37b6ae7c85dec10f
sai <saithilak0607@gmail.com>
2026-02-27 16:05:19 +0530

    JavaScript basics

---
+++ javascript/script.js
@@ -0,0 +1,47 @@
+// console.log("hello")
+// // variable can reinitialize and reassign
+// var a;
+// a=10;
+// var a;
+// a=20;
+// // rewinitialize
+// let b;
+// b=10;
+// // can't reinitialize and reassign
+// const c=20;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+var str="CSD"
+console.log(str)
+
+var obj={
+    name:"sai",
+    dept:"CSD"
+}
+console.log(obj);
+console.log(typeof(obj));
+// Arthimatic operater
+var a=10;
+var b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+// relational operater
+// var c=10
+// var d="10";
+// console.log(c==d);
+// console.log(c===d);
+// console.log(c!=d);
+// console.log(c>d);
+// console.log(c<d);
+// logical operater
+var a=true;
+var b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!b);
